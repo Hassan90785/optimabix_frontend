95da82b5923307f5329367ed669570f0924da781
Hassan <hassanali.naqvi@ajsoftware.com.pk>
2025-01-16 13:57:36 +0500

    Added header and footer

--- src/app/components/admin/admin/admin.component.ts
+++ src/app/components/admin/admin/admin.component.ts
@@ -1,15 +1,38 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {PanelMenu} from 'primeng/panelmenu';
+import {Menubar} from 'primeng/menubar';
+import {Toolbar} from 'primeng/toolbar';
+import {Button} from 'primeng/button';
+import {Sidebar} from 'primeng/sidebar';
+import {Menu} from 'primeng/menu';
 
 @Component({
   selector: 'app-admin',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    PanelMenu,
+    Menubar,
+    Toolbar,
+    Button,
+    Sidebar,
+    Menu
   ],
   templateUrl: './admin.component.html',
   standalone: true,
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  visibleSidebar1: boolean = false;
 
+  menuItems = [
+    { label: 'Dashboard', icon: 'pi pi-home', routerLink: ['/admin/dashboard'] },
+    { label: 'Users', icon: 'pi pi-users', routerLink: ['/admin/users'] },
+    { label: 'Roles', icon: 'pi pi-lock', routerLink: ['/admin/roles'] },
+    { label: 'Settings', icon: 'pi pi-cog', routerLink: ['/admin/settings'] },
+  ];
+
+  toggleSidebar(): void {
+    this.visibleSidebar1 = !this.visibleSidebar1;
+  }
 }
