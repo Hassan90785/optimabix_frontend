90bd58554c09aa2ed47f3318e360478e70d1d482
Hassan <hassanali.naqvi@ajsoftware.com.pk>
2025-02-23 18:19:58 +0500

    adedd a staging env for demo

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://optimabix.com:4789/staging/v1",
+  uploadUrl: "https://optimabix.com:8020/uploads/",
+  appName: "Optimabix"
+};
